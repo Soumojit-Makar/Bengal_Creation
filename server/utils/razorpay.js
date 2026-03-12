34fc14cdcd384cb8ad8b663238f3d45630f3ea73
Soumojit-Makar <ssoumojitmakar1234@gmail.com>
2026-03-12 20:13:52 +0530

    update payment related things

--- server/utils/razorpay.js
+++ server/utils/razorpay.js
@@ -1,6 +1,6 @@
 
 const Razorpay = require("razorpay");
-
+require("dotenv").config();
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY,
   key_secret: process.env.RAZORPAY_SECRET
