d26a81f848ec74397155449c0345b2e0ec255cb6
Soumojit-Makar <ssoumojitmakar1234@gmail.com>
2026-03-15 13:28:53 +0530

    fix api url in crm

--- bengal-crm/src/services/api.js
+++ bengal-crm/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.bengalcreations.in/api';
+const BASE_URL =  process.env.REACT_APP_API_URL|| 'https://api.bengalcreations.in/api';
 
 const api = axios.create({ baseURL: BASE_URL });
 
